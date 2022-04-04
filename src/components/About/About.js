b2f187e99c91870e4854d33342c40c0aefd8dbbf
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-04-04 21:47:41 +0600

    fixcing netlify problems

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ import './About.css';
 const About = () => {
     return (
         <div>
-            <h2 className='review-heading'>Drone review BD</h2>
+            <h2 className='review-heading py-5'>Drone review BD</h2>
             Drone review BD are all about drone. Where we review all drone in the market and take feedback from the user about drone experience. 
         </div>
     );
